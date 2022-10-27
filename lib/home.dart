55223d27534636656e0bb580f430bfe9d9bba8bf
malekfouda2 <97452787+malekfouda2@users.noreply.github.com>
2022-10-27 21:18:47 +0200

    home third update

--- lib/home.dart
+++ lib/home.dart
@@ -19,6 +19,7 @@ class Home extends StatefulWidget {
 
   @override
   State<Home> createState() => _MyStatefulWidgetState();
+  //State<Home> createState() => Drawer();
 }
 
 class _MyStatefulWidgetState extends State<Home> {
@@ -73,6 +74,62 @@ class _MyStatefulWidgetState extends State<Home> {
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
         onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class Drawer extends StatefulWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Navigation Drawer',
+        ),
+        backgroundColor: const Color(0xff764abc),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+              ),
+              title: const Text('Page 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.train,
+              ),
+              title: const Text('Page 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
       ),
     );
   }
